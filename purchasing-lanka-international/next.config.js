4abddfa9907c131875bce986963780cdc5e7db46
Oshadha Samarasinghe <124981711+OKSamarasinghe@users.noreply.github.com>
2025-05-19 22:57:51 +0530

    new

---
+++ purchasing-lanka-international/next.config.js
@@ -0,0 +1,40 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https', 
+        hostname: '*.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'aspireonline.lk',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = nextConfig;

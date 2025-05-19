d3dea64a24c057c5d0a4aea52c083ab0e850504e
Oshadha Samarasinghe <124981711+OKSamarasinghe@users.noreply.github.com>
2025-05-19 23:17:04 +0530

    update

--- purchasing-lanka-international/next.config.ts
+++ purchasing-lanka-international/next.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
@@ -37,4 +38,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
